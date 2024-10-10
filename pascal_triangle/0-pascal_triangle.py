c7ee201a79cf7a097a02839bf30ad0e4ab88436d
David Cyubahiro <156189366+DavidDau@users.noreply.github.com>
2024-10-10 09:50:00 +0200

    P8P Validation

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -14,16 +14,16 @@ def pascal_triangle(n):
     Pascal's Triangle by iteratively
     calculating the values of each row
     based on the previous row.
-    """ 
+    """
     if n <= 0:
         return[]
 
-    triangle=[]
- 
+    triangle = []
+
     for i in range(n):
         row = [1] * (i + 1)  # Start with a row of ones
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
-        
-    return triangle    
+
+    return triangle
