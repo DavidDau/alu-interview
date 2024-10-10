b5c94c4cf725b50b1c6dffeb72b69efc9b8d0048
David Cyubahiro <156189366+DavidDau@users.noreply.github.com>
2024-10-10 09:47:36 +0200

    Update 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -18,11 +18,12 @@ def pascal_triangle(n):
     if n <= 0:
         return[]
 
-triangle=[]
+    triangle=[]
  
     for i in range(n):
         row = [1] * (i + 1)  # Start with a row of ones
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
+        
     return triangle    
