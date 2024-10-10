82f41257fef0349201826ae062ea85f57f66e258
David Cyubahiro <156189366+DavidDau@users.noreply.github.com>
2024-10-10 09:44:57 +0200

    Update 0-pascal_triangle.py (P8P)

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -7,23 +7,22 @@ Returns an empty list if n <= 0
 You can assume n will be always an integer
 """
 
+
 def pascal_triangle(n):
     """
     This code effectively generates
     Pascal's Triangle by iteratively
-    calculating the values of each row 
+    calculating the values of each row
     based on the previous row.
     """ 
     if n <= 0:
-        return []
-    
-    triangle = []
-    
+        return[]
+
+triangle=[]
+ 
     for i in range(n):
         row = [1] * (i + 1)  # Start with a row of ones
         for j in range(1, i):
-            row[j] = triangle[i-1][j-1] + triangle[i-1][j]  # Calculate the inner values
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
-    
-    return triangle
-    
+    return triangle    
